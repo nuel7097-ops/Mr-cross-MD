0fa73aa4450e9950cad0c43414959adaa73837ff
nuel7097-ops <nuel7097@gmail.com>
2026-06-24 03:40:04 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+global.owner = ['2348063898506'] // owner number
+global.connect = true // false = QR, true = pairing code
+global.autoonline = true
+global.detectCall = true
+global.welcome = false
+global.public = true
+
+global.url = "https://t.me/cross006"
+global.url2 = "https://t.me/cross006"
+
+global.packname = "slayqueen md"
+global.author = "𝐃𝐄𝐕-𝐈𝐋༄𝐌𝐑.𝐂𝐑𝐎𝐒𝐒"
